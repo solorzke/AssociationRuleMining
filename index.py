b74a21e5e3833795f58b0a7fa82173ee0d15c05d
Kevin Solorzano <solorzke@gmail.com>
2018-04-16 20:59:06 -0400

    Import Libraries

---
+++ index.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import re
+import ssl
+import os
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import codecs
+
+
+print("Hello")

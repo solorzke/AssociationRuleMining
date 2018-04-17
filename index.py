28813de95b382c6311f9fb102905d85834cd3b9a
Kevin Solorzano <solorzke@gmail.com>
2018-04-16 21:27:30 -0400

    Defined 'join' function

--- index.py
+++ index.py
@@ -8,10 +8,17 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import codecs
 
+#check if two (k-1)-itemsets need to be joined into a k-itemset
 def need_join(itemSet1, itemSet2):
     for i in range(len(itemSet1)-2):
+        #check if all items except the last ones are identical
         if itemSet1[i] != itemSet2[i]:
             return(False)
     return(True)
+
+def join(itemSet1, itemSet2):
+    itemSet3 = itemSet1
+    itemSet3 = itemSet3 + itemSet2
+    return(itemSet3)
 
 print(need_join("dfdf", "dfdf"))
